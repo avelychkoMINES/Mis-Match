9e643c2ccdbd16d555870212cb4f78c132207b9c
Anastasia Velychko <avelychko@mines.edu>
2023-03-13 14:28:18 -0600

    added navhost

--- app/src/main/java/com/csci448/avelychko/mis_match/presentation/navigation/MisMatchNavHost.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/presentation/navigation/MisMatchNavHost.kt
@@ -3,16 +3,18 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.csci448.avelychko.mis_match.presentation.navigation.IScreenSpec
+import com.csci448.avelychko.mis_match.presentation.viewmodel.MisMatchViewModel
 
 @Composable
-fun MisMatchNavHost(navController: NavHostController, misMatchViewModel: MisMatchViewModel) {
+fun MisMatchNavHost(navController: NavHostController, viewModel: MisMatchViewModel) {
     NavHost( navController = navController, startDestination = IScreenSpec.root ) {
         navigation(IScreenSpec.startDestination, IScreenSpec.root) {
             IScreenSpec.allScreens.forEach { screen ->
                 if(screen != null) {
                     composable(route = screen.route, ) {
                         screen.Content(
-                            misMatchViewModel = misMatchViewModel,
+                            viewModel = viewModel,
                             navController = navController
                         )
                     }
