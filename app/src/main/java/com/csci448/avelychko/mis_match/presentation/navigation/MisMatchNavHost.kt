4f47b347932345fac9b3d4a7094823e5ac5ebfea
stlaurentel <83246818+stlaurentel@users.noreply.github.com>
2023-03-13 14:24:03 -0600

    Added imports to NavHost

--- app/src/main/java/com/csci448/avelychko/mis_match/presentation/navigation/MisMatchNavHost.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/presentation/navigation/MisMatchNavHost.kt
@@ -1,3 +1,9 @@
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+
 @Composable
 fun MisMatchNavHost(navController: NavHostController, misMatchViewModel: MisMatchViewModel) {
     NavHost( navController = navController, startDestination = IScreenSpec.root ) {
