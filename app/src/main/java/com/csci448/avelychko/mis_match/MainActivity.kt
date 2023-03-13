af9ac0460221afbe9ca493ed36fe18bf15a3bbc7
Anastasia Velychko <avelychko@mines.edu>
2023-03-13 14:50:08 -0600

    updated main activity

--- app/src/main/java/com/csci448/avelychko/mis_match/MainActivity.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.csci448.avelychko.mis_match
 
+import MisMatchNavHost
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,11 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.csci448.avelychko.mis_match.presentation.viewmodel.MisMatchViewModel
 import com.csci448.avelychko.mis_match.ui.theme.MisMatchTheme
 import com.csci448.mis_match_start.presentation.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val viewModel = MisMatchViewModel();
         super.onCreate(savedInstanceState)
         setContent {
             MisMatchTheme {
@@ -23,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    MisMatchScreen(viewModel)
                 }
             }
         }
@@ -31,14 +35,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting() {
-    //HomeScreen()
+fun MisMatchScreen(viewModel: MisMatchViewModel) {
+    val navController = rememberNavController()
+    MisMatchNavHost(navController = navController, viewModel = viewModel)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val viewModel = MisMatchViewModel();
     MisMatchTheme {
-        Greeting()
+        MisMatchScreen(viewModel = viewModel)
     }
 }
