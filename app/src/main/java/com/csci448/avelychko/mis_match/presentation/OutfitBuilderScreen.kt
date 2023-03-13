eea49edf0a5fa8b8b3c1ba687be32438d24a07fe
stlaurentel <83246818+stlaurentel@users.noreply.github.com>
2023-03-13 17:11:48 -0600

    Update OutfitBuilderScreen

--- app/src/main/java/com/csci448/avelychko/mis_match/presentation/OutfitBuilderScreen.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/presentation/OutfitBuilderScreen.kt
@@ -1,10 +1,12 @@
 package com.csci448.avelychko.mis_match.ui.theme
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -22,57 +24,75 @@ import com.csci448.avelychko.mis_match.R
 fun OutfitBuilderView(onLogoClicked: () -> Unit) {
     val context = LocalContext.current
 
-    Column(modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween) {
-        CenterAlignedTopAppBar(title = {
-            Text("Mis-Match!",
+    Column() {
+        CenterAlignedTopAppBar(title = {Text("Mis-Match!",
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.ExtraBold,
             fontSize = 36.sp,
-            fontFamily = FontFamily.Serif) },
+            fontFamily = FontFamily.Serif)},
             colors = TopAppBarDefaults.centerAlignedTopAppBarColors(Color(red = 199, green = 173, blue = 127)),
             modifier = Modifier.clickable { onLogoClicked() }
         )
         Divider(thickness = 2.dp, color = Color.Black)
 
-        //ARROW & IMAGE
-        Row(modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween) {
-            Column() {
-                Spacer(modifier = Modifier.height(300.dp))
-                IconButton(onClick = { Toast.makeText(context, "Shows previous top", Toast.LENGTH_SHORT).show() }) {
-                    Icon(painter = painterResource(id = R.drawable.baseline_chevron_left_24), contentDescription = "")
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .weight(0.8f)
+                .background(color = Color(red = 225, green = 208, blue = 191)),
+        ) {
+            //ARROW & IMAGE
+            Row(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween) {
+                Column() {
+                    Spacer(modifier = Modifier.height(220.dp))
+                    IconButton(onClick = { Toast.makeText(context, "Shows previous top", Toast.LENGTH_SHORT).show() }) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_chevron_left_24), contentDescription = "")
+                    }
+                    Spacer(modifier = Modifier.height(150.dp))
+                    IconButton(onClick = { Toast.makeText(context, "Shows previous bottoms", Toast.LENGTH_SHORT).show() }) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_chevron_left_24), contentDescription = "")
+                    }
+                    Spacer(modifier = Modifier.height(150.dp))
+                    IconButton(onClick = { Toast.makeText(context, "Shows previous shoes", Toast.LENGTH_SHORT).show() }) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_chevron_left_24), contentDescription = "")
+                    }
                 }
-                Spacer(modifier = Modifier.height(150.dp))
-                IconButton(onClick = { Toast.makeText(context, "Shows previous bottoms", Toast.LENGTH_SHORT).show() }) {
-                    Icon(painter = painterResource(id = R.drawable.baseline_chevron_left_24), contentDescription = "")
-                }
-                Spacer(modifier = Modifier.height(200.dp))
-                IconButton(onClick = { Toast.makeText(context, "Shows previous shoes", Toast.LENGTH_SHORT).show() }) {
-                    Icon(painter = painterResource(id = R.drawable.baseline_chevron_left_24), contentDescription = "")
+                Column() {
+                    Spacer(modifier = Modifier.height(220.dp))
+                    IconButton(onClick = { Toast.makeText(context, "Shows next top", Toast.LENGTH_SHORT).show() }) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_chevron_right_24), contentDescription = "")
+                    }
+                    Spacer(modifier = Modifier.height(150.dp))
+                    IconButton(onClick = { Toast.makeText(context, "Shows next bottoms", Toast.LENGTH_SHORT).show() }) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_chevron_right_24), contentDescription = "",)
+                    }
+                    Spacer(modifier = Modifier.height(150.dp))
+                    IconButton(onClick = { Toast.makeText(context, "Shows next shoes", Toast.LENGTH_SHORT).show() }) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_chevron_right_24), contentDescription = "")
+                    }
                 }
             }
-            Column() {
-                Spacer(modifier = Modifier.height(300.dp))
-                IconButton(onClick = { Toast.makeText(context, "Shows next top", Toast.LENGTH_SHORT).show() }) {
-                    Icon(painter = painterResource(id = R.drawable.baseline_chevron_right_24), contentDescription = "")
-                }
-                Spacer(modifier = Modifier.height(150.dp))
-                IconButton(onClick = { Toast.makeText(context, "Shows next bottoms", Toast.LENGTH_SHORT).show() }) {
-                    Icon(painter = painterResource(id = R.drawable.baseline_chevron_right_24), contentDescription = "")
-                }
-                Spacer(modifier = Modifier.height(200.dp))
-                IconButton(onClick = { Toast.makeText(context, "Shows next shoes", Toast.LENGTH_SHORT).show() }) {
-                    Icon(painter = painterResource(id = R.drawable.baseline_chevron_right_24), contentDescription = "")
-                }
+            //HEART
+
+            }
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color(red = 225, green = 208, blue = 191))) {
+            IconButton(modifier = Modifier.padding(5.dp),
+                onClick = {
+                    Toast.makeText(context, "Adds to saved outfits", Toast.LENGTH_SHORT).show()
+                }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_favorite_border_24),
+                    contentDescription = "",
+                    tint = Color.Red
+                )
             }
         }
-        //HEART
-        IconButton(modifier = Modifier.padding(5.dp),
-            onClick = { Toast.makeText(context, "Adds to saved outfits", Toast.LENGTH_SHORT).show() }) {
-            Icon(painter = painterResource(id = R.drawable.baseline_favorite_border_24), contentDescription = "")
         }
-    }
+
 }
 
 @Preview(showBackground = true)
