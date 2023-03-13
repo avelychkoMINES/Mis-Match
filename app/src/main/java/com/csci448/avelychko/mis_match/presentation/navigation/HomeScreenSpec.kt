87b26b6fe520e7344c4037e6e90ef2c3d7396410
stlaurentel <83246818+stlaurentel@users.noreply.github.com>
2023-03-13 14:30:32 -0600

    Update HomeScreenSpec

--- app/src/main/java/com/csci448/avelychko/mis_match/presentation/navigation/HomeScreenSpec.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/presentation/navigation/HomeScreenSpec.kt
@@ -1,4 +1,23 @@
 package com.csci448.avelychko.mis_match.presentation.navigation
 
-object HomeScreenSpec {
+object HomeScreenSpec : IScreenSpec {
+    override val route: String = "home"
+
+    @Composable
+    override fun Content(viewModel: MisMatchViewModel,
+                         navController: NavController) {
+        HomeScreen(viewModel = quizlerViewModel,
+            onXXX = {
+            navController.navigate("1")
+        }, onYYY =
+        {
+            navController.navigate("2")
+        }, onAAA =
+        {
+            navController.navigate("3")
+        }, onBBB =
+        {
+            navController.navigate("4")
+        })
+    }
 }
