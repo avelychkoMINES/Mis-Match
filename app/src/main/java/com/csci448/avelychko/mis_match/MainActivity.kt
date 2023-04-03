21a08039aac79fef66659c99476caa9aa79b8d29
Anastasia Velychko <avelychko@mines.edu>
2023-04-03 14:40:58 -0600

    added camera in manifest

--- app/src/main/java/com/csci448/avelychko/mis_match/MainActivity.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/MainActivity.kt
@@ -3,6 +3,7 @@ package com.csci448.avelychko.mis_match
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,15 +11,37 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.FileProvider
 import androidx.navigation.compose.rememberNavController
 import com.csci448.avelychko.mis_match.presentation.navigation.MisMatchNavHost
 import com.csci448.avelychko.mis_match.presentation.viewmodel.MisMatchViewModel
 import com.csci448.avelychko.mis_match.ui.theme.MisMatchTheme
 import com.csci448.mis_match_start.presentation.HomeScreen
+import java.io.File
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val viewModel = MisMatchViewModel();
+
+        val takePhotoLauncher = registerForActivityResult(
+            ActivityResultContracts.TakePicture()
+        ) { didTakePhoto ->
+            if(didTakePhoto) {
+                val photograph = Photograph(photoName)
+                viewModel.addPhotograph(photograph)
+            }
+        }
+
+        takePicture = {
+            photoName = "IMG_${Date()}.JPG"
+            val photoFile = File(this@MainActivity, photoName)
+            val photoUri = FileProvider.getUriForFile(this@MainActivity,
+                "edu.mines.csci448.examples.camera.fileprovider",
+                photoFile)
+            takePhotoLauncher.launch(photoUri)
+        }
+
         super.onCreate(savedInstanceState)
         setContent {
             MisMatchTheme {
--- app/src/main/java/com/csci448/avelychko/mis_match/presentation/viewmodel/MisMatchViewModel.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/presentation/viewmodel/MisMatchViewModel.kt
@@ -1,4 +1,7 @@
 package com.csci448.avelychko.mis_match.presentation.viewmodel
 
 class MisMatchViewModel {
+    fun addPhotograph(photograph: Any) {
+
+    }
 }
