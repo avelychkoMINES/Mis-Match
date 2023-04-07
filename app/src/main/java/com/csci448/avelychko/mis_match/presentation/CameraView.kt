617de8f681eb9b62bb988257ef59d31fb752891b
stlaurentel <83246818+stlaurentel@users.noreply.github.com>
2023-04-07 15:04:17 -0600

    Added imports

--- app/src/main/java/com/csci448/avelychko/mis_match/presentation/CameraView.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/presentation/CameraView.kt
@@ -34,6 +34,27 @@ import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.Executor
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.ImageCapture
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.android.example.cameraxapp.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import android.widget.Toast
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.core.Preview
+import androidx.camera.core.CameraSelector
+import android.util.Log
 
 private fun startCamera(context: Context) {
    val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
