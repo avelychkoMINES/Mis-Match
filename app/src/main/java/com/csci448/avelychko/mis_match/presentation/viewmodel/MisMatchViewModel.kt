965e576da3d9af822ae79ba0947d915ff0624526
Anastasia Velychko <avelychko@mines.edu>
2023-04-10 15:02:15 -0600

    view model

--- app/src/main/java/com/csci448/avelychko/mis_match/presentation/viewmodel/MisMatchViewModel.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/presentation/viewmodel/MisMatchViewModel.kt
@@ -1,13 +1,21 @@
 package com.csci448.avelychko.mis_match.presentation.viewmodel
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import com.csci448.avelychko.mis_match.data.Photograph
+import com.csci448.avelychko.mis_match.data.database.Picture
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class MisMatchViewModel {
     private val mTopPictureList: List<Photograph> = emptyList()
     private val mBottomPictureList: List<Photograph> = emptyList()
     private val mShoesPictureList: List<Photograph> = emptyList()
-
+    val selectedTopState: MutableState<Picture?> = mutableStateOf(null);
+    val selectedBottomState: MutableState<Picture?> = mutableStateOf(null);
+    val selectedShoeState: MutableState<Picture?> = mutableStateOf(null);
+    var indexTop: Int = 0;
+    var indexBottom: Int = 0;
+    var indexShoe: Int = 0;
 
 
     fun addPhotograph(photograph: Photograph) {
