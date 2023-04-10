c0e1549389f7b328ace209d59cbd67a9189938cf
Emma <83246818+stlaurentel@users.noreply.github.com>
2023-04-10 15:29:34 -0600

    fixed moving to prev/next item

--- app/src/main/java/com/csci448/avelychko/mis_match/presentation/viewmodel/MisMatchViewModel.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/presentation/viewmodel/MisMatchViewModel.kt
@@ -21,30 +21,48 @@ class MisMatchViewModel(val topList: List<Picture>, val bottomList: List<Picture
     
     fun moveToNextTop() {
         indexTop++
+        if (indexTop >= topList.length()) {
+            indexTop = 0
+        }
         selectedTopState.value = topList[indexTop]
     }
     
     fun moveToNextBottom() {
         indexBottom++
+        if (indexBottom >= bottomList.length()) {
+            indexBottom = 0
+        }
         selectedBottomState.value = bottomList[indexBottom]
     }
     
     fun moveToNextShoe() {
         indexShoe++
+        if (indexShoe >= shoeList.length()) {
+            indexShoe = 0
+        }
         selectedShoeState.value = shoeList[indexShoe]
     }
     
     fun moveToPrevTop() {
         indexTop--
+        if (indexTop < 0) {
+            indexTop = topList.length - 1
+        }
         selectedTopState.value = topList[indexTop]
     }
     
     fun moveToPrevBottom() {
         indexBottom--
+        if (indexBottom < 0) {
+            indexBottom = bottomList.length - 1
+        }
         selectedBottomState.value = bottomList[indexBottom]
     }
     
     fun moveToPrevShoe() {
+        if (indexShoe < 0) {
+            indexShoe = shoeList.length - 1
+        }
         indexShoe--
         selectedShoeState.value = shoeList[indexShoe]
     }
