5993be8e514f57d075d2e4653665ac7638becb82
Anastasia Velychko <avelychko@mines.edu>
2023-04-28 15:58:09 -0600

    updated viewModel and factory

--- app/src/main/java/com/csci448/avelychko/mis_match/presentation/navigation/IScreenSpec.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/presentation/navigation/IScreenSpec.kt
@@ -5,7 +5,7 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.camera.core.ImageCapture
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
-import com.csci448.avelychko.mis_match.presentation.viewmodel.MisMatchViewModel
+import com.csci448.avelychko.mis_match.presentation.viewmodel.PhotographViewModel
 import com.csci448.avelychko.mis_match.util.CameraUtility
 
 sealed interface IScreenSpec {
@@ -19,7 +19,7 @@ sealed interface IScreenSpec {
 
     @Composable
     fun Content(
-        viewModel: MisMatchViewModel, navController: NavController,
+        viewModel: PhotographViewModel, navController: NavController,
         activity: Activity,
         cameraUtility: CameraUtility,
         permissionLauncher: ActivityResultLauncher<Array<String>>
--- app/src/main/java/com/csci448/avelychko/mis_match/presentation/viewmodel/PhotographViewModel.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/presentation/viewmodel/PhotographViewModel.kt
@@ -1,3 +1,74 @@
+//package com.csci448.avelychko.mis_match.presentation.viewmodel
+//
+//import androidx.compose.runtime.MutableState
+//import androidx.compose.runtime.mutableStateOf
+//import com.csci448.avelychko.mis_match.data.Photograph
+//import com.csci448.avelychko.mis_match.data.PhotographRepository
+//import com.csci448.avelychko.mis_match.data.database.Picture
+//import kotlinx.coroutines.flow.MutableStateFlow
+//
+//class PhotographViewModel(val repo: PhotographRepository) {
+//    val selectedTopState: MutableState<Picture?> = mutableStateOf(null);
+//    val selectedBottomState: MutableState<Picture?> = mutableStateOf(null);
+//    val selectedShoeState: MutableState<Picture?> = mutableStateOf(null);
+//    var indexTop: Int = 0;
+//    var indexBottom: Int = 0;
+//    var indexShoe: Int = 0;
+//
+//
+//    fun addPhotograph(photograph: Photograph) {
+//        repo.addPhotograph(photograph)
+//    }
+//
+//    fun moveToNextTop() {
+//        indexTop++
+//        if (indexTop >= topList.size) {
+//            indexTop = 0
+//        }
+//        selectedTopState.value = topList[indexTop]
+//    }
+//
+//    fun moveToNextBottom() {
+//        indexBottom++
+//        if (indexBottom >= bottomList.size) {
+//            indexBottom = 0
+//        }
+//        selectedBottomState.value = bottomList[indexBottom]
+//    }
+//
+//    fun moveToNextShoe() {
+//        indexShoe++
+//        if (indexShoe >= shoeList.size) {
+//            indexShoe = 0
+//        }
+//        selectedShoeState.value = shoeList[indexShoe]
+//    }
+//
+//    fun moveToPrevTop() {
+//        indexTop--
+//        if (indexTop < 0) {
+//            indexTop = topList.size - 1
+//        }
+//        selectedTopState.value = topList[indexTop]
+//    }
+//
+//    fun moveToPrevBottom() {
+//        indexBottom--
+//        if (indexBottom < 0) {
+//            indexBottom = bottomList.size - 1
+//        }
+//        selectedBottomState.value = bottomList[indexBottom]
+//    }
+//
+//    fun moveToPrevShoe() {
+//        indexShoe--
+//        if (indexShoe < 0) {
+//            indexShoe = shoeList.size - 1
+//        }
+//        selectedShoeState.value = shoeList[indexShoe]
+//    }
+//}
+
 package com.csci448.avelychko.mis_match.presentation.viewmodel
 
 import android.util.Log
@@ -64,5 +135,17 @@ class PhotographViewModel(private val photographRepository: PhotographRepository
             mCurrentPhotographStateFlow.update { null }
         }
         photographRepository.deletePhotograph(photograph)
+    }
+
+    fun getTopPhoto(): List<Photograph> {
+        return photographRepository.top
+    }
+
+    fun getBottomPhoto(): List<Photograph> {
+        return photographRepository.bottom
+    }
+
+    fun getShoePhoto(): List<Photograph> {
+        return photographRepository.shoe
     }
 }
