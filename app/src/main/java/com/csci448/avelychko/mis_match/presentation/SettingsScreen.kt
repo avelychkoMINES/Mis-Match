f1737c5a9f37f438e531d0c6e4608d1cd4254f3b
Emma <83246818+stlaurentel@users.noreply.github.com>
2023-05-07 14:54:36 -0600

    Update SettingsScreen

--- app/src/main/java/com/csci448/avelychko/mis_match/presentation/SettingsScreen.kt
+++ app/src/main/java/com/csci448/avelychko/mis_match/presentation/SettingsScreen.kt
@@ -4,5 +4,26 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun SettingsScreen(){
+Column(modifier = Modifier.fillMaxSize()) {
 
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(red = 88, green = 76, blue = 109)),
+            horizontalArrangement = Arrangement.Center) {
+            Text(text = "Settings", fontSize = 30.sp,
+                color = Color(red=226, green=114, blue=91),
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.SemiBold)
+        }
+        Box(
+            //contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .weight(0.8f)
+                .background(color = Color(224, 224, 224)).fillMaxSize(),
+        ) {
+            Column() {
+              
+
+        }
+    }
 }
